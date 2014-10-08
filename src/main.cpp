1b5f48449d948db39ee82387e3c2c81ae6b4c3c2
Scott Mathis <golddustdeveloper@gmail.com>
2014-10-08 10:30:40 -0400

    changes

--- src/main.cpp
+++ src/main.cpp
@@ -2868,37 +2868,9 @@ bool InitBlockIndex() {
     // Only add the genesis block if not reindexing (in which case we reuse the one already on disk)
     if (!fReindex) {
         // Genesis Block:
+/*
 
-        // If genesis block hash does not match, then generate new genesis hash.
-          if (block.GetHash() != hashGenesisBlock)
-          {
-              printf("Searching for genesis block...\n");
-              // This will figure out a valid hash and Nonce if you're
-              // creating a different genesis block:
-              uint256 hashTarget = CBigNum().SetCompact(block.nBits).getuint256();
-              uint256 thash;
-
-              while(true)
-              {
-                  thash = scrypt_blockhash(BEGIN(block.nVersion));
-                  if (thash <= hashTarget)
-                      break;
-                  if ((block.nNonce & 0xFFF) == 0)
-                  {
-                      printf("nonce %08X: hash = %s (target = %s)\n", block.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-                  }
-                  ++block.nNonce;
-                  if (block.nNonce == 0)
-                  {
-                      printf("NONCE WRAPPED, incrementing time\n");
-                      ++block.nTime;
-                  }
-              }
-              printf("block.nTime = %u \n", block.nTime);
-              printf("block.nNonce = %u \n", block.nNonce);
-              printf("block.GetHash = %s\n", block.GetHash().ToString().c_str());
-
-
+*/
 
         // Genesis block
         const char* pszTimestamp = "'7th Heaven' star Stephen Collins exposed himself to young girls, report says";
